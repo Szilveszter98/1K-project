7242fb6c57bddf225e8c2522a16d8004f632d531
AnnaNordberg <56160770+AnnaNordberg@users.noreply.github.com>
2020-02-25 09:55:02 +0100

    update

--- views/signupForm.php
+++ views/signupForm.php
@@ -3,11 +3,7 @@
 <h1>
 Register
 <form method="POST" action="../includes/signup.php">
-<<<<<<< HEAD
-Username:<input type="text" name="username"><br/>
-=======
 Username:<input type="text" name="userName"required><br/>
->>>>>>> def41d76fef511fb31625a2e8c26c6fb887c7f73
 
 First name: <input type="text" name="firstName" required></br >
 Last name: <input type="text" name="lastName" required></br >
