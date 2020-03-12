533a1ab73ce5eaef25341657f9921eea4fddec9e
AnnaNordberg <56160770+AnnaNordberg@users.noreply.github.com>
2020-03-12 15:08:06 +0100

    updated css

--- views/blogForm.php
+++ views/blogForm.php
@@ -9,13 +9,13 @@
 <body>
 <div class="container">  
 <center>
-<h1 class="textToWritePost">
-Blog
+<p class="textToWritePost">
+<h1> Start writing your blog! </h1>
 <form method="POST" action="../includes/handleblog.php" >
-Title:<input type="text" name="title"required><br/>
-Description: <input type="text" name="description" required><br />
-<textarea name="blog_content" rows="20" cols="100"></textarea><br />
-Picture: <input class="submitWritePostFile" type="file" name="pictures" ><br />
+Title:<input type="text" name="title"required><br/> <br/>
+Description: <input type="text" name="description" required><br /> <br/>
+<textarea name="blog_content" rows="20" cols="100"></textarea><br /> <br/>
+Picture: <input class="submitWritePostFile" type="file" name="pictures" ><br /> <br/>
 <label for="category">Choose a category:</label>
 <select id="category" name="category">
   <option value="food">food</option>
@@ -24,8 +24,8 @@ Picture: <input class="submitWritePostFile" type="file" name="pictures" ><br />
   <option value="buisness">buisness</option>
 </select>
 <br/>
-<input class="submitWritePost" type="submit" name="submit" value="Send" /></h1>
-<hr>
+<input class="submitWritePost" type="submit" name="submit" value="Send" /></p>
+
 </form></center>
 
 </div>
